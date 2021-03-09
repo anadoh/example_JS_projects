02148b3adefff37d1a5151d388d034380b9f88d8
anadoh <68846949+anadoh@users.noreply.github.com>
2021-03-09 14:46:51 +0100

    script game over play again

--- hangman/script.js
+++ hangman/script.js
@@ -1,35 +1,107 @@
 const wordElement = document.getElementById("word");
 const wrongLettersEl = document.getElementById("wrong-letters");
-const playAgainBtn = document.getElementById("play-again");
+const playAgainBtn = document.getElementById("play-button");
 const popup = document.getElementById("popup-container");
 const notifiction = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
 
 const figureParts = document.querySelectorAll(".figure-part");
 
-const words = ["baby", "wolf", "cow", "flower"];
+
+
+
+const words = ['dog','cat','bubbles','door']
+console.log(words)
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ['o','c','w'];
+const correctLetters = ["o", "c", "w"];
 const wrongLetters = [];
 
 function displayWord() {
   wordElement.innerHTML = `
     ${selectedWord
-      .split('')
+      .split("")
       .map(
-        letter => `
+        (letter) => `
             <span class="letter">
-            ${correctLetters.includes(letter) ? letter : ''}
+            ${correctLetters.includes(letter) ? letter : ""}
             </span>`
       )
-      .join('')}
+      .join("")}
     `;
 
-    const innerWord = wordElement.innerText.replace(/\n/g, '');
-        if(innerWord === selectedWord) {
-            finalMessage.innerText = 'Congratulations! You won!';
-            popup.style.display = 'flex'
-        }
+  const innerWord = wordElement.innerText.replace(/\n/g, "");
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations! You won!";
+    popup.style.display = "flex";
+  }
 }
+
+function updateWrongLettersElement() {
+  //LETTERS
+  wrongLettersEl.innerHTML = `
+${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+`;
+  //PARTS
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+  //LOST GAME
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunately you lost.";
+    popup.style.display = "flex";
+  }
+}
+
+function showNotification() {
+  notifiction.classList.add("show");
+
+  setTimeout(() => {
+    notifiction.classList.remove("show");
+  }, 2500);
+}
+
+//if letter press
+window.addEventListener("keydown", (e) => {
+  //console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersElement();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
+// Restart game
+playAgainBtn.addEventListener('click', () => {
+  //Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+  displayWord();
+  updateWrongLettersElement();
+  popup.style.display = 'none'
+}) 
+
 displayWord();
