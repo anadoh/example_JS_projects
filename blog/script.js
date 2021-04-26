3be997dbdb20f398fa074c3e2f5c01808e8c5fce
anadoh <68846949+anadoh@users.noreply.github.com>
2021-04-26 10:41:56 +0200

    script js

--- blog/script.js
+++ blog/script.js
@@ -1,67 +1,73 @@
-const postContainer = document.getElementById('post-container');
-const loading = document.querySelector('.loader')
-const filter = document.getElementById('filter')
+const postContainer = document.getElementById("post-container");
+const loading = document.querySelector(".loader");
+const filter = document.getElementById("filter");
 
 const limit = 5;
-let page = 1;
+var page = 1;
 
 async function getPosts() {
-const res = await fetch(`https://api.punkapi.com/v2/beers?page=${page}&per_page=${limit}`);
+  const res = await fetch(
+    `https://api.punkapi.com/v2/beers?page=${page}&per_page=${limit}`
+  );
 
-const data = await res.json();
-return data;
+  const data = await res.json();
+  return data;
 }
 
-
 async function showPosts() {
-    const posts = await getPosts();
+  const posts = await getPosts();
 
-    posts.forEach(post=> {
-        const postEl = document.createElement('div'); 
-        postEl.classList.add('post');
-        postEl.innerHTML = `
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `
         <div class="number">${post.id}</div>
             <div class="post-info">
                 <h2 class="post-title">${post.name}</h2>
                 <p class="post-body">${post.description}</p>
             </div>
         `;
-        postContainer.appendChild(postEl);
-    })
-   
-
+    postContainer.appendChild(postEl);
+  });
 }
-function showLoading () {
-    loading.classList.add('show');
-    setTimeout(()=>{
-        loading.classList.remove('show')
-        setTimeout(()=> {
-            page++;
-            showPosts();
-        },300)
-    }, 1000);
-};
+function showLoading() {
+  loading.classList.add("show");
+  setTimeout(() => {
+    loading.classList.remove("show");
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
 
 function filterPosts(e) {
-    const term = e.target.value.toUpperCase();
-    const posts = document.querySelectorAll('.post');
+  const term = e.target.value.toLowerCase();
+  const posts = document.querySelectorAll(".post");
 
-    posts.forEach( pos t=> 
-        const title = post.querySelector('.post-title').innerText;
-        const body = post.querySelector('.post-body').innerText;
-         ) 
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText;
+    const body = post.querySelector(".post-body").innerText;
 
+    if (
+      title.toLowerCase().indexOf(term) > -1 ||
+      body.toLowerCase().indexOf(term) > -1
+    ) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
 }
-
 
 showPosts();
 
-window.addEventListener('scroll', () => {
-    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-    if(scrollTop + clientHeight >= scrollHeight - 5) {
-        showLoading();
-    }
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
 });
 
-filter.addEventListener('input',filterPosts);
+filter.addEventListener("input", filterPosts);
